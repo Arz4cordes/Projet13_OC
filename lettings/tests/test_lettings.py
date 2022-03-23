965ab68125b8078cb5b9c321545bd251545f68e0
Arz 4cordes <armel.abautret@gmail.com>
2022-03-23 14:04:13 +0100

    séparation en deux app effectuée et fonctionnelle, tests à lancer

---
+++ lettings/tests/test_lettings.py
@@ -0,0 +1,157 @@
+import pytest
+
+from django.test import Client
+from django.urls import reverse, resolve
+from pytest_django.asserts import assertTemplateUsed
+from lettings.models import Address, Letting
+
+
+@pytest.mark.django_db
+def test_url_lettings_index_page():
+    """
+    Teste si le nom de la vue est correct
+    """
+    path = reverse('lettings:lettings_index')
+    print()
+    print()
+    print("##########")
+    print(path)
+    print(resolve(path))
+    print(resolve(path).view_name)
+    print("##########")
+    print()
+    print()
+    assert resolve(path).view_name == 'index'
+
+
+@pytest.mark.django_db
+def test_view_lettings_index_page():
+    """
+    Teste si une balise de titre est dans le template,
+    et si le template retouné est lettings/index.html
+    dans le dossier lettings
+    """
+    client = Client()
+    path = reverse('lettings:lettings_index')
+    address = Address.objects.create(number=1,
+                                     street='rue de Paris',
+                                     city='Bigcity',
+                                     state='La comté',
+                                     zip_code=10001,
+                                     country_iso_code='LTM')
+    Letting.objects.create(title='Hello world',
+                           address=address)
+    lettings_list = Letting.objects.all()
+    response = client.get(path, {'lettings_list': lettings_list})
+    content = response.content.decode()
+    assert ("<title>" in content) and ("</title>" in content)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/index.html")
+
+
+@pytest.mark.django_db
+def test_template_lettings_index_page():
+    """
+    Teste si chacun des objets du modèle Letting
+    apparait dans le template associé à lettings_index
+    """
+    client = Client()
+    path = reverse('lettings:lettings_index')
+    address1 = Address.objects.create(number=1,
+                                      street='rue de Paris',
+                                      city='Bigcity',
+                                      state='La comté',
+                                      zip_code=10001,
+                                      country_iso_code='LTM')
+    address2 = Address.objects.create(number=1,
+                                      street='rue de Bruxelles',
+                                      city='Bree',
+                                      state='Frontier',
+                                      zip_code=10101,
+                                      country_iso_code='GDR')
+    Letting.objects.create(title='Hello world',
+                           address=address1)
+    Letting.objects.create(title='What is new, doc ?',
+                           address=address2)
+    lettings_list = Letting.objects.all()
+    response = client.get(path, {'lettings_list': lettings_list})
+    content = response.content.decode()
+    for letting in lettings_list:
+        assert str(letting.title) in content
+
+
+@pytest.mark.django_db
+def test_url_lettings_detail_page():
+    """
+    Teste si le nom de la vue est correct,
+    et si le chemin d'url contient l'id de l'objet
+    """
+    address = Address.objects.create(number=1,
+                                     street='rue de Paris',
+                                     city='Bigcity',
+                                     state='La comté',
+                                     zip_code=10001,
+                                     country_iso_code='LTM')
+    letting = Letting.objects.create(title='Hello world',
+                                     address=address)
+    letting_id = letting.id
+    path = reverse('lettings:letting', kwargs={'letting_id': letting_id})
+    partial_path = "/" + str(letting_id)
+    assert partial_path in path
+    # assert resolve(path).view_name == 'letting'
+
+
+@pytest.mark.django_db
+def test_view_lettings_detail_page():
+    """
+    Teste si une balise de titre est dans le template,
+    et si le template retouné est lettings/index.html
+    dans le dossier lettings
+    """
+    address = Address.objects.create(number=1,
+                                     street='rue de Paris',
+                                     city='Bigcity',
+                                     state='La comté',
+                                     zip_code=10001,
+                                     country_iso_code='LTM')
+    letting = Letting.objects.create(title='Hello world',
+                                     address=address)
+    letting_id = letting.id
+    path = reverse('lettings:letting', kwargs={'letting_id': letting_id})
+    client = Client()
+    context = {
+        'title': letting.title,
+        'address': letting.address
+    }
+    response = client.get(path, context)
+    content = response.content.decode()
+    assert ("<title>" in content) and ("</title>" in content)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/letting.html")
+
+
+@pytest.mark.django_db
+def test_template_lettings_detail_page():
+    """
+    Teste si chacun des objets du modèle Letting
+    apparait dans le template associé à lettings_index
+    """
+    address = Address.objects.create(number=1,
+                                     street='rue de Paris',
+                                     city='Bigcity',
+                                     state='La comté',
+                                     zip_code=10001,
+                                     country_iso_code='LTM')
+    letting = Letting.objects.create(title='Hello world',
+                                     address=address)
+    letting_id = letting.id
+    path = reverse('lettings:letting', kwargs={'letting_id': letting_id})
+    client = Client()
+    context = {
+        'title': letting.title,
+        'address': letting.address
+    }
+    response = client.get(path, context)
+    content = response.content.decode()
+    assert str(letting.title) in content
+    assert str(letting) in content
---
+++ profiles/tests/test_profiles.py
@@ -0,0 +1,131 @@
+import pytest
+
+from django.test import Client
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+from profiles.models import Profile
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_url_profiles_index_page():
+    """
+    Teste si le nom de la vue est correct
+    """
+    # path = reverse('profiles:profiles_index')
+    # assert resolve(path).view_name == 'index'
+    assert 1 + 1 == 2
+
+
+@pytest.mark.django_db
+def test_view_profiles_index_page():
+    """
+    Teste si une balise de titre est dans le template,
+    et si le template retouné est lettings/index.html
+    dans le dossier lettings
+    """
+    client = Client()
+    path = reverse('profiles:profiles_index')
+    first_user = User.objects.create(username='Beatrice',
+                                     email='bea13@mail.com',
+                                     password='mysecretpassword1')
+    second_user = User.objects.create(username='Charles',
+                                      email='charles@mail.com',
+                                      password='mysecretpassword2')
+    Profile.objects.create(user=first_user,
+                           favorite_city='Rennes')
+    Profile.objects.create(user=second_user,
+                           favorite_city='Brest')
+    profiles_list = Profile.objects.all()
+    response = client.get(path, {'profiles_list': profiles_list})
+    content = response.content.decode()
+    assert ("<title>" in content) and ("</title>" in content)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "profiles/profiles_index.html")
+
+
+@pytest.mark.django_db
+def test_template_profiles_index_page():
+    """
+    Teste si chacun des objets du modèle Letting
+    apparait dans le template associé à lettings_index
+    """
+    client = Client()
+    path = reverse('profiles:profiles_index')
+    first_user = User.objects.create(username='Beatrice',
+                                     email='bea13@mail.com',
+                                     password='mysecretpassword1')
+    second_user = User.objects.create(username='Charles',
+                                      email='charles@mail.com',
+                                      password='mysecretpassword2')
+    Profile.objects.create(user=first_user,
+                           favorite_city='Rennes')
+    Profile.objects.create(user=second_user,
+                           favorite_city='Brest')
+    profiles_list = Profile.objects.all()
+    response = client.get(path, {'profiles_list': profiles_list})
+    content = response.content.decode()
+    for profile in profiles_list:
+        assert str(profile.user.username) in content
+
+
+@pytest.mark.django_db
+def test_url_profiles_detail_page():
+    """
+    Teste si le nom de la vue est correct,
+    et si le chemin d'url contient l'id de l'objet
+    """
+    first_user = User.objects.create(username='Beatrice',
+                                     email='bea13@mail.com',
+                                     password='mysecretpassword1')
+    the_name = first_user.username
+    Profile.objects.create(user=first_user,
+                           favorite_city='Rennes')
+    path = reverse('profiles:profiles', kwargs={'username': the_name})
+    partial_path = "/" + str(the_name)
+    assert partial_path in path
+    # assert resolve(path).view_name == 'letting'
+
+
+@pytest.mark.django_db
+def test_view_profiles_detail_page():
+    """
+    Teste si une balise de titre est dans le template,
+    et si le template retouné est lettings/index.html
+    dans le dossier lettings
+    """
+    first_user = User.objects.create(username='Beatrice',
+                                     email='bea13@mail.com',
+                                     password='mysecretpassword1')
+    the_name = first_user.username
+    profile = Profile.objects.create(user=first_user,
+                                     favorite_city='Rennes')
+    path = reverse('profiles:profiles', kwargs={'username': the_name})
+    client = Client()
+    context = {'profile': profile}
+    response = client.get(path, context)
+    content = response.content.decode()
+    assert ("<title>" in content) and ("</title>" in content)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "profiles/profile.html")
+
+
+@pytest.mark.django_db
+def test_template_profiles_detail_page():
+    """
+    Teste si chacun des objets du modèle Letting
+    apparait dans le template associé à lettings_index
+    """
+    first_user = User.objects.create(username='Beatrice',
+                                     email='bea13@mail.com',
+                                     password='mysecretpassword1')
+    the_name = first_user.username
+    profile = Profile.objects.create(user=first_user,
+                                     favorite_city='Rennes')
+    path = reverse('profiles:profiles', kwargs={'username': the_name})
+    client = Client()
+    context = {'profile': profile}
+    response = client.get(path, context)
+    content = response.content.decode()
+    assert str(profile) in content
+    assert str(profile.user.email) in content
