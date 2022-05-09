20180b18edc92738aca8f440a4b0561516a2a616
Arz 4cordes <armel.abautret@gmail.com>
2022-05-09 13:12:15 +0200

    projet finalisé

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from oc_lettings_site import views
-
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    return division_by_zero
+from oc_lettings_site import views, sentry_error_example
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sentry-debug/', trigger_error),
+    path('sentry-debug/', sentry_error_example.trigger_error),
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
     path('', include('lettings.urls')),
